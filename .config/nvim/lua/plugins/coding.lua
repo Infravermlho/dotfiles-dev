8c0ae9146d25b5290da2eea8264b885fbfef06e3
Kolibri <lucassouzasilva74@hotmail.com>
2024-12-09 18:28:37 -0300

    init

---
+++ .config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				-- Lua
+				"stylua",
+				"selene",
+				"luacheck",
+				-- Shell
+				"shellcheck",
+				"shfmt",
+				-- Javascript
+				"tailwindcss-language-server",
+				"vue-language-server",
+				"typescript-language-server",
+				"css-lsp",
+				-- Python
+				"black",
+				"isort",
+				-- Misc
+				"yaml-language-server",
+				"prettierd",
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				["lua"] = { "stylua" },
+				["fish"] = { "fish_indent" },
+				["sh"] = { "shfmt" },
+				["python"] = { "isort", "black" },
+				["yaml"] = { "prettierd" },
+				["json"] = { "prettierd" },
+				["markdown"] = { "prettierd" },
+				["vue"] = { "prettierd" },
+			},
+		},
+	},
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Kolibri's development env dotfiles
+
+Arch Linux dotfiles automated with Ansible, probably going to automate windows too later
+
+Tested mostly in WSL2
+
+# Missing:
+
+**SSH Agents and forwarding**
+**TMUX**
+**Lazygit(?)**
+
+Windows Ansible bootstrap
+
+# Contents:
+
+Fish + Starship
+TMUX
+Neovim
+Keychain
+Ansible script for complete bootstrap on Arch
+
+# Install:
+
+didn't test this yet lol but should probably work
+
+```bash
+sudo pacman -S git ansible
+mkdir -r ~/Dotfiles/dev/
+git clone git@github.com:Infravermlho/dotfiles-dev.git ~/Dotfiles/dev/
+ansible-playbook bootstrap.yml
+```
